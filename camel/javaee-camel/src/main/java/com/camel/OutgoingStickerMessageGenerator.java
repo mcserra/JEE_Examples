810d60a6387ca6fb1157be1e3d6c50cb4cb813ce
Miguel Serra <Miguel.Serra@criticaltechworks.com>
2020-01-05 13:00:23 +0000

    Test addGame with camel-telegram

--- camel/javaee-camel/src/main/java/com/camel/OutgoingStickerMessageGenerator.java
+++ camel/javaee-camel/src/main/java/com/camel/OutgoingStickerMessageGenerator.java
@@ -3,13 +3,12 @@ package com.camel;
 import javax.inject.Named;
 
 import org.apache.camel.Body;
+import org.apache.camel.component.telegram.model.EditMessageTextMessage;
 import org.apache.camel.component.telegram.model.OutgoingStickerMessage;
 
 @Named
 public class OutgoingStickerMessageGenerator {
     public OutgoingStickerMessage sendMessage(@Body String stickerId) {
-        return OutgoingStickerMessage.createWithFileId(
-            "CAADBAADEQADmDVxAkmg3XnDZam0FgQ", null, null, null
-        );
+        return OutgoingStickerMessage.createWithFileId("CAADAgADOQADfyesDlKEqOOd72VKAg", null,null, null);
     }
 }
